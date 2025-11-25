2c61e67006a87f4195511d0ec308fface79db6d9
Dongmin Kim <dmdmk94@gmail.com>
2025-11-25 13:17:15 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,10 +1,5 @@
 SELECT user_id, product_id
 FROM online_sale
 GROUP BY user_id, product_id
-HAVING COUNT(product_id) > 1
-ORDER BY user_id, product_id DESC;
-
-
-
-# 재구매 여부를 구하는 방법..?
-# 회원이 산 제품의 개수
+HAVING COUNT(*) > 1
+ORDER BY user_id ASC, product_id DESC;
